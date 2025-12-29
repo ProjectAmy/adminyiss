776d7c32f608d63fc88506865f5c98bb7cdbf0e9
amysidra <amysidra@gmail.com>
2025-12-29 22:03:50 +0700

    feat: Add Google login button component and integrate Google authentication.

--- components/login-button.tsx
+++ components/login-button.tsx
@@ -1,5 +1,6 @@
-import { FaG } from 'react-icons/fa6'
 import { signIn } from '@/auth'
+import { GoogleSubmitBtn } from './google-submit-btn'
+
 
 export const LoginGoogleButton = () => {
     return (
@@ -10,13 +11,7 @@ export const LoginGoogleButton = () => {
             }}
             className="w-full"
         >
-            <button
-                type="submit"
-                className="flex items-center justify-center gap-2 w-full bg-blue-700 text-white font-medium py-3 px-6 text-base rounded-md hover:bg-blue-600 cursor-pointer"
-            >
-                <FaG className='size-6' />
-                Login dengan Google
-            </button>
+            <GoogleSubmitBtn />
         </form>
     )
 }
